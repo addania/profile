1172640fbc4c6a693c41ddde4cc55e36756262b4
addania <mirka.schw@gmail.com>
2020-02-02 20:58:40 +0100

    addint title page title and backgground image

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,14 @@
 import React from "react"
+import "./index.css";
+import hero from './img/hero.jpg';
 
-export default () => <div>Hello world!</div>
+export default () => 
+<div>
+  <div class="image">
+     <img src={hero} style={{width: "100%"}} alt="hero image" />
+
+ <h2 style={{ fontSize: `72px` }} className="one"><span>Hi, I am Mia! I work as a Front-End Developer  {/*<span class='spacer'></span>*/}</span></h2>
+ <h2 style={{ fontSize: `72px` }} className="two"><span>I am passionate about building and designing user interfaces :)  {/*<span class='spacer'></span>*/}</span></h2>
+  </div>
+
+</div>
