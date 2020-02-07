4cbc7ac5ecd293d2cadae00912c78dd19819e50d
addania <mirka.schw@gmail.com>
2020-02-07 08:28:39 +0100

    adding bootstrap rows and columns

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,16 +2,27 @@ import React from "react"
 import "./index.css";
 import hero from './img/hero.jpg';
 import logo from './img/logo.png';
+import { Row, Col, Container } from "react-bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default () => 
 <div>
-  <div class="image">
-
+  <div>
      <img src={hero} style={{width: "100%"}} alt="hero" />
+     <Row>
+     <Col sm={12} style ={{position: "absolute", top: "120px", width: "100%", textAlign: "center" }}>
      <div style={{width:"100%"}} className="centerImage"><img src={logo} style={{width: "45px"}} className="logo" alt="logo" /></div>
-     
-     <h2 style={{ fontSize: `72px` }} className="one"><span>Hi, I am Mia! I work as a Front-End Developer  {/*<span class='spacer'></span>*/}</span></h2>
-     <h2 style={{ fontSize: `72px` }} className="two"><span>I am passionate about building and designing user interfaces :)  {/*<span class='spacer'></span>*/}</span></h2>
+     </Col>
+     </Row>
+     <Row>
+     <Col sm={12} style ={{position: "absolute", top: "100px" }}>
+     <h2 className="title"><span>Hi, I am Mia! I work as a Front-End Developer</span></h2>
+     </Col>
+     </Row>
+     <Row>
+     <Col sm={12} style ={{position: "absolute", top: "150px" }}>
+     <h2 className="subtitle"><span>I am passionate about building and designing user interfaces :)</span></h2>
+     </Col>
+     </Row>
   </div>
-
 </div>
